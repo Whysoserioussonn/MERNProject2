9f18ed7d7d0965693ca648f488418555121bcc4c
gzod1016 <60858393+gzod1016@users.noreply.github.com>
2022-11-11 22:50:46 -0500

    setup middleware

--- server.js
+++ server.js
@@ -1,7 +1,22 @@
+const cookieParser = require('cookie-parser')
 const express = require('express')
 const app = express()
 const path = require('path')
+const errorHandler = require('./middleware/errorHandler')
+const cors = require('cors')
+const corsOptions = require('./config/corsOptions')
+const { logger } = require('./middleware/logger')
+
+
 const PORT = process.env.PORT || 5000
+
+app.use(logger)
+
+app.use(cors(corsOptions))
+
+app.use(express.json())
+
+app.use(cookieParser())  // third party middleware
 
 app.use('/', express.static(path.join(__dirname, 'public')))   //telling express where to find static files like images using __dirname means look inside the folder that we are in which is /public folder
 
@@ -17,6 +32,8 @@ app.all('*', (req, res) => {
       res.type('txt').send('404 Not Found')
   }
 })
+
+app.use(errorHandler)
 
 app.listen(PORT,  () => console.log(`Server running on port ${PORT}`))
 

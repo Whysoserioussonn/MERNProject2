508a995ccbb3239793eb472f48f315bfe72450ff
gzod1016 <60858393+gzod1016@users.noreply.github.com>
2022-11-12 00:49:32 -0500

    connected mongodb

--- server.js
+++ server.js
@@ -6,12 +6,16 @@ const path = require('path')
 const errorHandler = require('./middleware/errorHandler')
 const cors = require('cors')
 const corsOptions = require('./config/corsOptions')
-const { logger } = require('./middleware/logger')
+const { logger, logEvents } = require('./middleware/logger')
+const connectDB = require('./config/dbConn')
+const mongoose = require('mongoose')
 
 
 const PORT = process.env.PORT || 5000
 
 console.log(process.env.NODE_ENV)
+
+connectDB()
 
 app.use(logger)
 
@@ -38,5 +42,12 @@ app.all('*', (req, res) => {
 
 app.use(errorHandler)
 
+mongoose.connection.once('open', () => {
+    console.log('Connected to MongoDB')
 app.listen(PORT,  () => console.log(`Server running on port ${PORT}`))
+})
 
+mongoose.connection.on('error', err => {
+    console.log(err)
+    logEvents(`${err.no}: ${err.code}\t${err.syscall}\t${err.hostname}`, 'mongoErrLog.log')
+})

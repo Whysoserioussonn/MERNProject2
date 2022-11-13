a1b947f2c7b5bcc39656b54e76c1f703b7252878
gzod1016 <60858393+gzod1016@users.noreply.github.com>
2022-11-13 15:35:49 -0500

    reinstall repaired files that include working acesss token and refresh token

--- controllers/authController.js
+++ controllers/authController.js
@@ -31,13 +31,13 @@ const login = asyncHandler(async (req, res) => {
           }
       },
       process.env.ACCESS_TOKEN_SECRET,
-      { expiresIn: '1m' }
+      { expiresIn: '10s' }
   )
 
   const refreshToken = jwt.sign(
       { "username": foundUser.username },
       process.env.REFRESH_TOKEN_SECRET,
-      { expiresIn: '1d' }
+      { expiresIn: '30s' }
   )
 
   // Create secure cookie with refresh token 
